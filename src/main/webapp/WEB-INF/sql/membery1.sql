dd87eb3f4f5bdf53d08d411e658a3b46c404b6eb
lllilyk <ym0402kkk@naver.com>
2023-06-15 16:23:34 +0900

    memberController 잘 실행되는 버전

--- src/main/webapp/WEB-INF/sql/membery1.sql
+++ src/main/webapp/WEB-INF/sql/membery1.sql
@@ -48,13 +48,64 @@ SELECT * FROM Book;
 ALTER TABLE Feed
 ADD FOREIGN KEY (writer) REFERENCES Member(id);
 
-SELECT * FROM Feed;
-USE membery;
+SELECT * FROM Member;
 
-DELETE FROM Member
-WHERE id IS NULL;
+DESC Member;
+
+-- 권한 테이블
+CREATE TABLE MemberAuthority(
+	memberId VARCHAR(20) NOT NULL,
+    authority VARCHAR(30) NOT NULL,
+    FOREIGN KEY (memberId) REFERENCES Member(id),
+    PRIMARY KEY (memberId, authority)
+);
+
+DESC Host;
+
+INSERT INTO MemberAuthority 
+VALUES ('membow', 'admin');
+
+-----------------------------------
+INSERT INTO MemberAuthority (memberId, authority)
+SELECT 'host'
+FROM Host h
+WHERE (SELECT Host.id host FROM Member LEFT JOIN Host ON Member.id = Host.memberId);
+-----------------------------------
 
 SELECT * FROM Member;
-DELETE FROM Member WHERE inserted = '2023-06-11 22:42:59';
+SELECT * FROM Host;
+SELECT * FROM MemberAuthority;
 
-DESC File;
+-- membow	admin
+INSERT INTO MemberAuthority (memberId, authority) 
+SELECT memberId, 'host' FROM Host WHERE memberId IS NOT NULL;
+
+SELECT * 
+FROM Member m LEFT JOIN MemberAuthority ma ON m.id = ma.memberId
+WHERE m.id = 'membow';
+
+SELECT * 
+FROM Member JOIN Host ON Member.id = Host.memberId;
+
+SELECT * FROM Pet;
+SELECT * FROM Member;
+
+SELECT m.*, ma.authority, h. CONCAT('/', p.id, '/', p.photo) profileImage
+			FROM Member m LEFT JOIN Pet p ON m.defaultPetId = p.id
+						  LEFT JOIN MemberAuthority ma ON m.id = ma.memberId
+                          LEFT JOIN Host h ON m.id = h.memberId
+			WHERE m.id = 'abc';
+DESC Member;
+
+SELECT * FROM MemberAuthority;
+DESC MemberAuthority;
+
+CREATE TABLE Test(
+	memberId VARCHAR(30),
+    authority VARCHAR(30)
+);
+
+INSERT INTO Test (authority)
+SELECT 'host'
+FROM Host h
+WHERE (SELECT Host.id host FROM Member LEFT JOIN Host ON Member.id = Host.memberId);
